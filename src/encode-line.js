3a623ba8a77c573c14e0dba09183bfab49432a0b
Andrej-Kautsevich <andrej.kautsevich@yandex.by>
2023-12-19 20:33:31 +0300

    feat: compleate encode line (Tue, Dec 19, 2023 08:33:31 PM)

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (!str) return '';
+
+  let result = '';
+  let currentSymbol = str[0];
+  let counter = 0;
+  
+  for (let i = 0; i < str.length; i++) {
+    const symbol = str[i];
+    if (symbol === currentSymbol) {
+      counter += 1;
+    } else {
+      result += counter > 1 ? counter + currentSymbol : currentSymbol;
+      counter = 1;
+      currentSymbol = symbol;
+    }
+  }
+  result += counter > 1 ? counter + currentSymbol : currentSymbol;
+  
+  return result;
 }
 
 module.exports = {
