b7ba022a006ee6a5ad128dca19f1afe015c8b4f8
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2023-12-25 00:59:31 +0300

    feat: compleate vigenere-cipher (Mon, Dec 25, 2023 12:59:31 AM)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,90 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction) {
+    this.direction = direction;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    let result = ''
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const upperCaseMessage = message.toUpperCase();
+
+    if (upperCaseMessage.match(/[A-Z]/g)) {
+      const plainText = upperCaseMessage.match(/[A-Z]/g).join('');
+      const keyString = key.repeat(Math.ceil(plainText.length / key.length)).slice(0, plainText.length).toUpperCase();
+      let cipherText = '';
+
+
+      for (let i = 0; i < plainText.length; i++) {
+        const index = (alphabet.indexOf(plainText[i]) + alphabet.indexOf(keyString[i])) % alphabet.length;
+        const letter = alphabet[index]
+        cipherText += letter;
+      }
+
+      for (let i = 0; i < upperCaseMessage.length; i++) {
+        if (alphabet.includes(upperCaseMessage[i])) {
+          result += cipherText[0];
+          cipherText = cipherText.slice(1);
+        } else {
+          result += upperCaseMessage[i];
+        }
+      }
+    } else {
+      result = upperCaseMessage;
+    }
+
+    if (this.direction === false) {
+      result = result.split('').reverse().join('');
+    }
+
+    return result;
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    let result = ''
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const upperCaseMessage = encryptedMessage.toUpperCase();
+
+    if (upperCaseMessage.match(/[A-Z]/g)) {
+      const cipherText = upperCaseMessage.match(/[A-Z]/g).join('');
+      const keyString = key.repeat(Math.ceil(cipherText.length / key.length)).slice(0, cipherText.length).toUpperCase();
+      let plainText = '';
+
+      for (let i = 0; i < cipherText.length; i++) {
+        const index = (alphabet.length + alphabet.indexOf(cipherText[i]) - alphabet.indexOf(keyString[i])) % alphabet.length;
+        const letter = alphabet[index];
+        plainText += letter;
+      }
+
+      for (let i = 0; i < upperCaseMessage.length; i++) {
+        if (alphabet.includes(upperCaseMessage[i])) {
+          result += plainText[0];
+          plainText = plainText.slice(1);
+        } else {
+          result += upperCaseMessage[i];
+        }
+      }
+    } else {
+      result = upperCaseMessage;
+    }
+
+    if (this.direction === false) {
+      result = result.split('').reverse().join('');
+    }
+
+    return result;
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
